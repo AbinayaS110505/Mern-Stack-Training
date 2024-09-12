bd9a3a823f547915d2d9e29c5c13207d86d14ab1
Abinaya S <abinayas495100@gmail.com>
2024-09-11 20:43:11 -0700

    Final Resume

---
+++ in.js
@@ -0,0 +1,14 @@
+
+    let a=10;
+        console.log(a);
+        a=2000000000000000000;
+        
+        console.log(a);
+        let datajs=
+        {name: "Abinaya S",
+            age:20,
+            Deg:"B.Tech"
+        }
+        console.log(datajs.age);
+        
+     

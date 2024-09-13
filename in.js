abf230d928daf7b199625e37f784727a191a9110
Abinaya S <abinayas495100@gmail.com>
2024-09-13 15:34:39 -0700

    updated

--- in.js
+++ in.js
@@ -1,14 +1,20 @@
-
-    let a=10;
-        console.log(a);
-        a=2000000000000000000;
-        
-        console.log(a);
-        let datajs=
-        {name: "Abinaya S",
-            age:20,
-            Deg:"B.Tech"
-        }
-        console.log(datajs.age);
-        
-     
+function fun1()
+{
+    if(c==1){
+        console.log("function 3")}
+    else{
+    console.log("function 1")
+     c=1;}
+}
+const fun2 = () =>
+{
+      for(let i=0;i<3;i++)
+    console.log("function 2")
+}
+const fun3=() => {
+    console.log(fun1());
+}
+var c=0;
+     fun1();
+     fun2();
+     fun3();
